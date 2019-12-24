8a2686b43d73f07f568ed9832e4b7b4c20e3dfc8
TM <gam@DESKTOP-0PUD35J>
2019-12-24 11:11:14 +0900

    20191224 骨組み

---
+++ docs/public/js/script.js
@@ -0,0 +1,40 @@
+$(function() {
+	$(document).ready(function() {
+		responsive();
+	});
+
+	var resizeTimer = null;
+	$(window).on('resize', function() {
+		clearTimeout(resizeTimer);
+		resizeTimer = setTimeout(function() {
+			responsive();
+		}, 200);
+	});
+
+	$('#left').on('click', function() {
+		if (parseInt($('#thumbnails').css('width')) > $(window).width() + (parseInt($('#thumbnails').css('left')) * -1)) {
+			$('#thumbnails').animate({
+				'left' : '-=240'
+			});
+		}
+		return false;
+	});
+	$('#right').on('click', function() {
+		if (parseInt($('#thumbnails').css('left')) < 0) {
+			$('#thumbnails').animate({
+				'left' : '+=240'
+			});
+		}
+		return false;
+	});
+});
+
+function responsive() {
+	if ($(window).width() > 580) {
+		$('#thumbnails').width(240 * $('#thumbnails').children('.thumbnail').length);
+		$('#thumbnails').find('a').attr('target', 'main');
+	} else {
+		$('#thumbnails').removeAttr('style');
+		$('#thumbnails').find('a').attr('target', 'blink');
+	}
+}
